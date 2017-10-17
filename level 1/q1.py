aef77e2fe3bace4c1da59b3e79914a935640d788
colopop <colopop@users.noreply.github.com>
2017-10-16 22:42:18 -0400

    Update q1.py

--- level 1/q1.py
+++ level 1/q1.py
@@ -1,1 +1,12 @@
+def answer(data, n):
+	#treating n==0 separately could improve performance with many workers
+	if n == 0:
+		return []
 
+	from collections import Counter
+	count = Counter()
+	#first pass counts total occurrences
+	for worker in data:
+		count[worker] += 1
+	#create and return the correct list
+	return [w for w in data if count[w] < n]
